ed755c76761d7176d77e090bd79861c5945765d6
kyle roberts <kyle.w.r@hotmail.com>
2017-07-31 14:57:19 +0000

    Added user agent

--- download_avatars.js
+++ download_avatars.js
@@ -6,9 +6,17 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var GITHUB_USER = "KwinstonRoberts";
-  var GITHUB_TOKEN = "d6abd9cb0c246238f18b067553e5b3a090df830c";
+  var GITHUB_TOKEN = "1fcd546c798967f1a00ad42e5fd59e6a5b9600bf";
   var url = `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors` 
-  console.log(url);
+  var options = {
+    url:url,
+    headers:{
+      'User-Agent': 'github avatar Downloader'
+    }
+  }
+  request(options,function(err,response,body){
+    return cb(err,body);
+  })
 }
 
 getRepoContributors("jquery","jquery", function(err,result){
