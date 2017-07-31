c39eb8257f7b49e2705c13939b6e68ab01d2112a
kyle roberts <kyle.w.r@hotmail.com>
2017-07-31 15:30:29 +0000

    added arguments and a write stream to fetch images

--- download_avatars.js
+++ download_avatars.js
@@ -2,27 +2,43 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-
+var fs = require('fs');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var GITHUB_USER = "KwinstonRoberts";
   var GITHUB_TOKEN = "1fcd546c798967f1a00ad42e5fd59e6a5b9600bf";
-  var url = `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors` 
-  var options = {
-    url:url,
-    headers:{
-      'User-Agent': 'github avatar Downloader'
-    },
-    json:true
+  if(!process.argv[2] || !process.argv[3]){
+    console.log("You must provide both a repo owner and the name of the repo as arguments.");
+  }else{
+    var url = `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors` 
+    var options = {
+      url:url,
+      headers:{
+        'User-Agent': 'github avatar Downloader'
+      },
+      json:true
+    }
+    request(options,function(err,response,body){
+      return cb(err,body);
+    });
   }
-  request(options,function(err,response,body){
-    return cb(err,body);
-  })
 }
 
-getRepoContributors("jquery","jquery", function(err,result){
-  console.log("Errors:", err);
-  for(x in result){
-    console.log(result[x].avatar_url);
+getRepoContributors(process.argv[2],process.argv[3], function(err,result){
+  if(!err){
+    for(x in result){
+      downloadImageByURL(result[x].avatar_url, `avatars/${result[x].login}`);
+    }
   }
 });
+
+function downloadImageByURL(url, filePath) {
+  request(url)
+    .on('error',function(err){
+      throw err;
+    })
+    .on('response',function(response){
+      console.log('fetched image at: ', url);
+    })
+    .pipe(fs.createWriteStream("./" + filePath + ".jpg"));
+  }
